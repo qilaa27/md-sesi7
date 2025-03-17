e086f2f20e23d45022cd81891c26ee0d33bedd66
qilaa27 <140408048+qilaa27@users.noreply.github.com>
2025-03-17 14:30:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,5 +57,10 @@ def main():
                vacuolisation_damage_basal_layer, spongiosis, saw_tooth_appearance_retes, follicular_horn_plug, perifollicular_parakeratosis,
                inflammatory_mononuclear_infiltrate, band_like_infiltrate, age]
 
+model_filename = "trained_model.pkl"
+model = load_model(model_filename)
+prediction = predict_with_model(model, user_input)
+st.write('The prediction output is: ', prediction)
+
 if __name__ == "__main__":
   main()
